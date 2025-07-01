df1630f0fbf04f66ad70d364887876edb969f015
Kola Praneeth <kola.praneeth2020@vitstudent.ac.in>
2025-07-02 01:08:55 +0530

    Production Error Fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,17 +11,19 @@ import { app,server } from './lib/socket.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production" 
+        ? false 
+        : "http://localhost:5173",
     credentials: true,
 }));
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -31,5 +33,5 @@ if (process.env.NODE_ENV === "production") {
 }
 server.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
-    connectDB()
+    connectDB();
 });
